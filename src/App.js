55946dc4f67c17881620f36649ea707e60b62d4b
Wendy Teo <wendyteo.wy@gmail.com>
2022-08-23 07:41:35 +1000

    added sun and mars

--- src/App.js
+++ src/App.js
@@ -3,31 +3,50 @@ import { useState } from 'react';
 import styles from './App.module.css';
 
 import earth from './images/earth.png'
+import mars from './images/mars.png'
 
 function App() {
 
-  const [isClicked, setIsClicked] = useState(null);
+  const [earthIsClicked, setEarthIsClicked] = useState(null);
+  const [marsIsClicked, setMarsIsClicked] = useState(null);
 
-  const clickHandler = () => {
-    setIsClicked((value) => !value);
-    console.log(isClicked);
+  const earthClickHandler = () => {
+    setEarthIsClicked((value) => !value);
   };
 
-  const description = (
+  const marsClickHandler = () => {
+    setMarsIsClicked((value) => !value);
+  };
+
+  const earthDescription = (
           <div className={styles.description}>
             <p>Earth</p>
           </div>
           );
+  
+  const marsDescription = (
+          <div className={styles.description}>
+            <p>Mars</p>
+          </div>
+          );     
 
   return (
     <div className={styles.solarSystemContainer}>
       <p>The Solar System</p>
-      <div className={styles.orbit}>
-        <div className={styles.planet}>
-          <img onClick={clickHandler} src={earth} alt="earth" />
+      {earthIsClicked && earthDescription}
+      {marsIsClicked && marsDescription}
+      <div id={styles.sun}>
+      </div>
+      <div className={styles.earthOrbit}>
+        <div className={styles.earthPlanet}>
+          <img onClick={earthClickHandler} src={earth} alt="earth" />
         </div>
       </div>
-      {isClicked && description}
+      <div className={styles.marsOrbit}>
+        <div className={styles.marsPlanet}>
+          <img onClick={marsClickHandler} src={mars} alt="mars" />
+        </div>
+      </div>
     </div>
   );
 }
